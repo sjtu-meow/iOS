bbbed733057c0840d10bd47a566ec47a19d4b31f
Wuwei Lin <vincentl13x@gmail.com>
2017-06-27 14:06:04 +0800

    Add moya endpoint closure

--- Meow/Networking.swift
+++ Meow/Networking.swift
@@ -11,5 +11,34 @@ import RxSwift
 import Alamofire
 
 class MeowAPIProvider {
+    open static var shared: MeowAPIProvider = MeowAPIProvider()
+
+    static func refresh() {
+        shared = MeowAPIProvider()
+    }
+    
+    let delegate: RxMoyaProvider<MeowAPI>
+    
+    let token: Token?
+    
+    private init() {
+        token = Token.load()
+        
+        let xToken:Token? = token 
+        let endpointClosure = { (target: MeowAPI) -> Endpoint<MeowAPI> in
+            
+            var endpoint = MoyaProvider.defaultEndpointMapping(for: target)
+            
+            if let xToken = xToken {
+                endpoint = endpoint.adding(httpHeaderFields: ["X-Access-Token": xToken.access])
+            }
+            
+            return endpoint
+        }
+        
+        delegate = RxMoyaProvider<MeowAPI>(endpointClosure: endpointClosure)
+
+    }
+
     
 }
