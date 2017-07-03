b4b959013e604c9c91a75c45304daf673db29aea
Wuwei Lin <vincentl13x@gmail.com>
2017-07-03 20:32:12 +0800

    Fix media model

--- Meow/Media.swift
+++ Meow/Media.swift
@@ -9,24 +9,22 @@
 
 import SwiftyJSON
 
-enum MediaType: Int {
-    case Image = 0, Video = 1
+enum MediaType: String {
+    case Image = "Image", Video = "Video"
 }
 
 struct Media {
     var type: MediaType?
-    var thumbnail: URL?
     var url: URL?
 }
 
 extension Media: JSONConvertible {
     static func fromJSON(_ json: JSON) -> Media? {
         var media = self.init()
-        guard let type = json["type"].int else { return nil}
+        guard let type = json["type"].string else { return nil}
         
         media.type = MediaType.init(rawValue: type)
         media.url <- json["url"]
-        media.thumbnail <- json["thumbnail"]
         return media
     }
 }
