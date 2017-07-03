ddcc139db0f3a48c47e2694842ceeee7c31870ac
Wuwei Lin <vincentl13x@gmail.com>
2017-07-03 15:26:56 +0800

    Fix signup

--- Meow/SignupViewController.swift
+++ Meow/SignupViewController.swift
@@ -74,7 +74,7 @@ class SignupViewController: UIViewController {
         //verify verfication code
         AVOSCloud.verifySmsCode(verificationCode, mobilePhoneNumber: phone){(succeeded, error) in
             if succeeded{
-                postSignupForm(p)
+                postSignupForm(phone:phone, password: password, validationCode: verificationCode)
             }
             else{
                 HUD.flash(.labeledError(title: "短信验证码错误", subtitle: nil), delay: 1)
