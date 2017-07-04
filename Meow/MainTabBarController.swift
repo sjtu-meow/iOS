01c2fb42c409034b7b9df5bcc4a7c646564c8c8b
anchovYu <aureole@sjtu.edu.cn>
2017-07-04 23:00:28 +0800

    what have i done

---
+++ Meow/MainTabBarController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabBarController.swift
+//  Meow
+//
+//  Created by 林树子 on 2017/7/4.
+//  Copyright © 2017年 喵喵喵的伙伴. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    override func viewDidLoad() {
+        
+    }
+
+}
