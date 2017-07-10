b29f13bb76003f3dd95dddd5e6f4cbe15a5fd672
Wuwei Lin <vincentl13x@gmail.com>
2017-07-10 10:28:36 +0800

    It works

--- Meow/ArticleDetailViewController.swift
+++ Meow/ArticleDetailViewController.swift
@@ -11,9 +11,11 @@ import UIKit
 import WebKit
 import RxSwift
 
-class ArticleDetailViewController: UITableViewController {
+class ArticleDetailViewController: UIViewController {
     let disposeBag = DisposeBag()
     
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var webviewCell: UITableViewCell!
     /* user profile info */
     @IBOutlet weak var avatarImageView: UIImageView!
     @IBOutlet weak var nicknameLabel: UILabel!
@@ -26,19 +28,22 @@ class ArticleDetailViewController: UITableViewController {
     var article: Article?
     
     override func viewDidLoad() {
-        tableView.tableFooterView = UIView(frame: CGRect.zero)
+        
         webview = ArticleWebView(fromSuperView: webViewContainer)
         
+        automaticallyAdjustsScrollViewInsets = false
+        
         //loadData()
-        webview.load(URLRequest(url:URL(string:"http://sjtu.edu.cn")!))
+        
         
         webview.heightChangingHandler = {
             [weak self] height in
-            if let that = self {
-                that.tableView.reloadRows(at: [IndexPath(row: 1, section: 0)], with: .none)
-            }
+            self?.scrollView.contentSize.height = height
+            //self?.view.setNeedsLayout()
+            self?.webview.heightConstraint.constant = height
         }
-        webview.updateHeight()
+        webview.load(URLRequest(url:URL(string:"https://tongqu.me")!))
+        //webview.updateHeight()
     }
     
     var content: String?
@@ -66,6 +71,7 @@ class ArticleDetailViewController: UITableViewController {
     @IBAction func goBack(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
     func updateView() {
         guard let article = self.article else { return }
         if let content = article.content {
@@ -80,13 +86,6 @@ class ArticleDetailViewController: UITableViewController {
         
     }
     
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.row == 0 {
-            return 0
-        }
-        return 100
-        //return UITableViewAutomaticDimension
-    }
-    
+   
 }
 
