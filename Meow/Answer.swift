2c953d1614638e8c46c98f30b4fbe4849fba394f
Wuwei Lin <vincentl13x@gmail.com>
2017-07-10 15:45:01 +0800

    Add a amazing answer list

--- Meow/Answer.swift
+++ Meow/Answer.swift
@@ -10,38 +10,35 @@ import SwiftyJSON
 
 struct Answer: ItemProtocol {
     var id: Int!
+    var questionId: Int!
     var type: ItemType!
     var profile: Profile!
     
     var questionTitle: String?
     var content: String!
-    //var questionId: Int!
-    var like: Int?  // FIXME: not empty
-    var comment: Int?
+    var likeCount: Int!
+    var commentCount: Int!
+    
 }
 
 extension Answer: JSONConvertible {
     static func fromJSON(_ json: JSON) -> Answer? {
-        guard let item = Item.fromJSON(json),
-              let content = json["content"].string
-              //let questionId = json["questionId"].int
-              //let like = json["like"].int,
-              //let comment = json["comment"].int
-            
-            else { return nil }
-        
         var answer = self.init()
+        let answerJson = json["answer"]
+        guard let item = Item.fromJSON(answerJson) else { return nil }
+        
         answer.id = item.id
         answer.type = item.type
         answer.profile = item.profile
         
-        answer.questionTitle <- json["questionTitle"]
-        answer.content = content
-        // answer.questionId = questionId
-        answer.like <- json["like"]
-        answer.comment <- json["comment"]
+        answer.questionTitle <- json["title"]
+        answer.questionId <- json["id"]
         
+        answer.content <- answerJson["content"]
+        
+        answer.likeCount <- answerJson["likeCount"]
+        answer.commentCount <- answerJson["commentCount"]
+       
         return answer
-    
     }
 }
---
+++ Meow/QuestionDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  QuestionDetailViewController.swift
+//  Meow
+//
+//  Created by 林武威 on 2017/7/10.
+//  Copyright © 2017年 喵喵喵的伙伴. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+
+class QuestionDetailViewController: UITableViewController {
+    
+}
