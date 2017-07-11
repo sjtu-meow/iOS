07ca5d5bc96a1ac9a727b498e5adfae88dce46ec
Wuwei Lin <vincentl13x@gmail.com>
2017-07-11 16:49:19 +0800

    Implement 'add question'

--- Meow/PostQuestionViewController.swift
+++ Meow/PostQuestionViewController.swift
@@ -6,11 +6,31 @@
 //
 
 import UIKit
+import RxSwift
 
 class PostQuestionViewController: UIViewController {
+    let disposeBag = DisposeBag()
     
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var contentTextField: UITextView!
+    
+    override func viewDidLoad() {
+        contentTextField.textContainer.lineBreakMode = .byWordWrapping
+    }
     @IBAction func cancel(_ sender: Any) {
         dismiss(animated: true, completion: nil)
+    }
+    @IBAction func postQuestion(_ sender: Any) {
+        guard let title = titleTextField.text, let content = contentTextField.text
+            else { return }
+        MeowAPIProvider.shared.request(.postQuestion(title: title, content: content))
+            .subscribe(onNext: {
+                [weak self]
+                _ in
+                self?.dismiss(animated: true, completion: nil)
+            })
+        .addDisposableTo(disposeBag)
     }
     
 }
