6d29099e1002adda82294a7f3f37903a3453f83b
TerCZ <t.chuzhe@qq.com>
2017-07-20 10:00:11 +0800

    Add comment cell.

---
+++ Meow/CommentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CommentTableViewCell.swift
+//  Meow
+//
+//  Created by 唐楚哲 on 2017/7/19.
+//  Copyright © 2017年 喵喵喵的伙伴. All rights reserved.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
